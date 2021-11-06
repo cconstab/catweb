58c77de405ba599a055b8fc3f8199c67d20abbfd
Colin Constable <colin@constable.cc>
2021-11-05 19:39:44 -0700

    Update update_atsign.dart
    bugs found

--- lib/services/update_atsign.dart
+++ lib/services/update_atsign.dart
@@ -1,7 +1,4 @@
 import 'package:at_commons/at_commons.dart';
-import 'package:at_app_flutter/at_app_flutter.dart' show AtEnv;
-import 'package:path_provider/path_provider.dart'
-    show getApplicationSupportDirectory;
 
 import 'package:at_client_mobile/at_client_mobile.dart';
 import 'package:hamlibui/services/formats.dart';
@@ -16,14 +13,17 @@ updateAtsign(HamRadio hamradio) async {
   atClient = atClientManager.atClient;
   currentAtsign = atClient.getCurrentAtSign();
   var currentAtsignNoAt = currentAtsign!.replaceAll('@', '');
+
   String radiourl =
       'https://wavi.ng/api?atp=${hamradio.radioName}.$currentAtsignNoAt@ai6bh&html=true';
   String wavi =
       '''{"label":"Listening on ${hamradio.radioName}","category":"DETAILS","type":"Text","value":"<iframe src=\\"$radiourl\\" sandbox=\\"allow-scripts\\"  style=\\"height:75px;width:1100px\\" title=\\"Iframe Example\\"></iframe>","valueLabel":""}''';
- 
+
   String message =
       '<!DOCTYPE html> <html> <head> <meta http-equiv="refresh" content="5" /><style> h1 {text-align: center;} </style> </head><body> <h1>${currentAtsign.toUpperCase()} using ${hamradio.radioName} listening on ${frequencyFormat(hamradio.vfoaFrequency.toString()).padRight(10)} ${hamradio.vfoaModulationMode} </h1> </body> </html>';
 
+  String qrt =
+      '<!DOCTYPE html> <html> <head> <meta http-equiv="refresh" content="5" /><style> h1 {text-align: center;} </style> </head><body> <h1>${currentAtsign.toUpperCase()} using ${hamradio.radioName} QRT for now </h1> </body> </html>';
   // Save radio Frequency and Mode
   var metaData = Metadata()
     ..isPublic = true
@@ -38,10 +38,10 @@ updateAtsign(HamRadio hamradio) async {
     ..key = hamradio.radioName
     ..sharedBy = currentAtsign
     ..sharedWith = null
-    // ..namespace = currentAtsignNoAt
     ..metadata = metaData;
 
   print('Updating: ' + key.toString() + '  :::  ' + message + ' Mhz');
+  //await atClient.delete(key);
   await atClient.put(key, message);
   atClientManager.syncService.sync();
   var test = await atClient.get(key);
@@ -51,19 +51,12 @@ updateAtsign(HamRadio hamradio) async {
     print('RADIO VALUE::: ' + test.value);
   }
 
-  var dir = await getApplicationSupportDirectory();
+  key = AtKey()
+    ..key = hamradio.radioName
+    ..sharedBy = currentAtsign
+    ..sharedWith = null
+    ..metadata = metaData;
 
-  var atclientpreference = AtClientPreference()
-    ..rootDomain = AtEnv.rootDomain
-    ..namespace = 'wavi.'
-    ..hiveStoragePath = dir.path
-    ..commitLogPath = dir.path
-    ..isLocalStoreRequired = true;
-
-  atClient.setPreferences(atclientpreference);
-  var pref = atClient.getPreferences();
-
-  print('PREF:: ' + pref!.namespace.toString());
   String wavikey = 'custom_' + hamradio.radioName + '.wavi';
 
   metaData = Metadata()
@@ -72,12 +65,12 @@ updateAtsign(HamRadio hamradio) async {
     ..namespaceAware = false
     // // one minute
     // ..ttl = 60000 ;
-   // One Hour
+    // One Hour
     ..ttl = 3600000;
   key = AtKey()
     ..key = wavikey
     ..sharedBy = currentAtsign
-   // ..namespace = 'wavi'
+    // ..namespace = 'wavi'
     ..metadata = metaData;
 
   print('+++++++++++++++++++++++++++++++');
