3c9b716fe755db3f4f1bad5aaf2eef48cecee34f
cconstab <colin@constable.cc>
2021-11-07 13:46:37 -0800

    Create public_radio_model.dart

---
+++ lib/models/public_radio_model.dart
@@ -0,0 +1,54 @@
+class HamRadio {
+  String radioName;
+  String radioUuid;
+  String? vfoaFrequency;
+  String? vfoaModulationMode;
+  String? vfoaOperatingMode;
+  String? vfobFrequency;
+  String? vfobModulationMode;
+  String? vfobOperatingMode;
+  String? vfoaFrequencyLast;
+  String? vfoaModulationModeLast;
+  String? vfoaOperatingModeLast;
+  String? vfobFrequencyLast;
+  String? vfobModulationModeLast;
+  String? vfobOperatingModeLast;
+
+  HamRadio({
+    required this.radioName,
+    required this.radioUuid,
+    this.vfoaFrequency = '0000000000',
+    this.vfoaModulationMode = '---',
+    this.vfoaOperatingMode = '---',
+    this.vfobFrequency = '0000000000',
+    this.vfobModulationMode = '---',
+    this.vfobOperatingMode = '---',
+    this.vfoaFrequencyLast = '0000000000',
+    this.vfoaModulationModeLast = '---',
+    this.vfoaOperatingModeLast = '---',
+    this.vfobFrequencyLast = '0000000000',
+    this.vfobModulationModeLast = '---',
+    this.vfobOperatingModeLast = '---',
+  });
+
+  HamRadio.fromJson(Map<String, dynamic> json)
+      : radioName = json['radioName'],
+        radioUuid = json['radioUuid'],
+        vfoaFrequency = json['vfoaFrequency'],
+        vfoaModulationMode = json['vfoaModulationMode'],
+        vfoaOperatingMode = json['vfoaOperatingMode'],
+        vfobFrequency = json['vfobFrequency'],
+        vfobModulationMode = json['vfobModulationMode'],
+        vfobOperatingMode = json['vfobOperatingMode'];
+
+  Map<String, dynamic> toJson() => {
+        'radioName': radioName,
+        'radioUuid': radioUuid,
+        'vfoaFrequency': vfoaFrequency,
+        'vfoaModulationMode': vfoaModulationMode,
+        'vfoaOperatingMode': vfoaOperatingMode,
+        'vfobFrequency': vfobFrequency,
+        'vfobModulationMode': vfobModulationMode,
+        'vfobOperatingMode': vfobOperatingMode,
+      };
+}
