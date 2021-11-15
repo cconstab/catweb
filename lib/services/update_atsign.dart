87785d4b7dea1d4ca1f5f41407fc437906a9db7a
Colin Constable <colin@constable.cc>
2021-11-15 07:59:58 -0800

    yay adaptive iFrame

--- lib/services/update_atsign.dart
+++ lib/services/update_atsign.dart
@@ -34,12 +34,15 @@ updateAtsign(HamRadio hamradio) async {
 //  String wavi =
 //       '''{"label":"Listening on ${hamradio.radioName}","category":"DETAILS","type":"Text","value":"<iframe src=\\"$radiourl\\"  style=\\"height:90px;width:900px\\"</iframe>","valueLabel":""}''';
 
- // String html = '''<!DOCTYPE html><html><head><meta name=\\"viewport\\" content=\\"width=device-width, initial-scale=1\\"><style>.container {  position: relative;  width: 100%;  overflow: hidden;  padding-top: 56.25%;} .responsive-iframe {  position: absolute;  top: 0;  left: 0;  bottom: 0;  right: 0;  width: 100%;  height: 100%;  border: none;</style></head><body><div class=\\"container\\">  <iframe class=\\"responsive-iframe\\" src=\\"$radiourl\\"></iframe></div></body</html>  ''';
- String html = '''<html><head><meta name=\\"viewport\\" content=\\"width=device-width, initial-scale=1\\"><style>.container {  position: relative;  width: 100%;  overflow: hidden;  padding-top: 10%;} .responsive-iframe {  position: absolute;  top: 0;  left: 0;  bottom: 0;  right: 0;  width: 100%;  height: 100%;  border: none;</style></head><body><div class=\\"container\\">  <iframe class=\\"responsive-iframe\\" src=\\"$radiourl\\"></iframe></div></body</html>  ''';
+  // String html = '''<!DOCTYPE html><html><head><meta name=\\"viewport\\" content=\\"width=device-width, initial-scale=1\\"><style>.container {  position: relative;  width: 100%;  overflow: hidden;  padding-top: 56.25%;} .responsive-iframe {  position: absolute;  top: 0;  left: 0;  bottom: 0;  right: 0;  width: 100%;  height: 100%;  border: none;</style></head><body><div class=\\"container\\">  <iframe class=\\"responsive-iframe\\" src=\\"$radiourl\\"></iframe></div></body</html>  ''';
+// String html = '''<html><head><meta name=\\"viewport\\" content=\\"width=device-width, initial-scale=1\\"><style>.container {  position: relative;  width: 100%;  overflow: hidden;  padding-top: 10%;} .responsive-iframe {  position: absolute;  top: 0;  left: 0;  bottom: 0;  right: 0;  width: 100%;  height: 100%;  border: none;</style></head><body><div class=\\"container\\">  <iframe class=\\"responsive-iframe\\" src=\\"$radiourl\\"></iframe></div></body</html>  ''';
+//String html = '''<html><head><meta name=\\"viewport\\" content=\\"width=device-width, initial-scale=1\\"><style>.container {  position: relative;  width: 100%;  overflow: hidden;  padding-top: 10%;} .responsive-iframe {  width: 100%;  height: 100%;  border: none;</style></head><body><div class=\\"container\\">  <iframe class=\\"responsive-iframe\\" src=\\"$radiourl\\"></iframe></div></body</html>  ''';
 
- String wavi =
-   '''{"label":"Listening on ${hamradio.radioName}","category":"DETAILS","type":"Text","value":"$html","valueLabel":""}''';
+  String html =
+      '''<html><head><meta name=\\"viewport\\" content=\\"width=device-width, initial-scale=1\\"><style>.container {  position: relative;  width: 100%;  overflow: hidden;  } .responsive-iframe {  position: absolute;  top: 0;  left: 0;  bottom: 0;  right: 0;  width: 100%;  height: 100%;  border: none;} .pt-10{padding-top: 10%;}</style></head><body><div class=\\"container pt-10\\">  <iframe class=\\"responsive-iframe\\" src=\\"$radiourl\\"></iframe></div></body</html>''';
 
+  String wavi =
+      '''{"label":"Listening on ${hamradio.radioName}","category":"DETAILS","type":"Text","value":"$html","valueLabel":""}''';
 
   var metaData = Metadata()
     ..isPublic = true
