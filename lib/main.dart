2364afe2f44152446746d8bc986080e9eaa2b47e
cconstab <colin@constable.cc>
2021-11-27 16:10:31 -0800

    working windows and Android
    .env file solved... Cannot migrate to at_app 4.0.0. issue raised with dev..

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,10 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:at_client_mobile/at_client_mobile.dart';
-import 'package:at_onboarding_flutter/at_onboarding_flutter.dart'
-    show Onboarding;
 import 'package:at_utils/at_logger.dart' show AtSignLogger;
 import 'package:path_provider/path_provider.dart'
     show getApplicationSupportDirectory;
-import 'package:at_app_flutter/at_app_flutter.dart' show AtEnv;
+import 'package:catweb/services/at_env.dart' show AtEnv;
 
 import 'package:catweb/screens/main_screen.dart';
 import 'package:catweb/screens/new_radio.dart';
